e9e99147dadac76655bae1c24abccd0d2817c93d
Alvaro De León <orochies@gmail.com>
2023-09-13 10:37:03 -0600

    add @capacitor-community/camera-preview

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { PhotosService } from './services/photos.service';
 import { CommonModule } from '@angular/common';
+import { CameraPreview, CameraPreviewOptions } from '@capacitor-community/camera-preview';
 
 @Component({
   selector: 'app-root',
@@ -14,11 +15,22 @@ export class AppComponent {
 
   photos: string[] = [];
 
+  cameraPreviewOptions: CameraPreviewOptions = {
+    position: 'rear',
+    height: 1920,
+    width: 1080
+  };
+
   constructor(private photosService: PhotosService) {
     this.photos = this.photosService.photos;
   }
 
   async takePhoto() {
     await this.photosService.addNewFoto();
+  }
+
+
+  takePhotoPreview() {
+    CameraPreview.start({ parent: "cameraPreview" });
   }
 }
